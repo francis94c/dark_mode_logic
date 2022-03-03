6ef41360ba07c571c91c4a5716bfe9b4efd082a4
Francis Ilechukwu <francis94c@gmail.com>
2022-03-03 16:53:04 +0100

    Dark mode logic test.

--- test/dark_mode_logic_test.dart
+++ test/dark_mode_logic_test.dart
@@ -1,12 +1,77 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:dark_mode_logic/dark_mode_logic.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
+  testWidgets(
+    'Can return default dark mode color',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: LayoutBuilder(
+            builder: (context, constraints) => Icon(
+              Icons.abc,
+              color: ColorGate.byMode(
+                  context: context, lightModeColor: Colors.blue),
+            ),
+          ),
+          theme: ThemeData(
+            brightness: Brightness.dark,
+          ),
+        ),
+      );
+
+      expect(
+          (tester.firstWidget(find.byType(Icon)) as Icon).color, Colors.white);
+    },
+  );
+
+  testWidgets(
+    'Can return dark mode color',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: LayoutBuilder(
+            builder: (context, constraints) => Icon(
+              Icons.abc,
+              color: ColorGate.byMode(
+                  context: context,
+                  lightModeColor: Colors.blue,
+                  darkModeColor: Colors.brown),
+            ),
+          ),
+          theme: ThemeData(
+            brightness: Brightness.dark,
+          ),
+        ),
+      );
+
+      expect(
+          (tester.firstWidget(find.byType(Icon)) as Icon).color, Colors.brown);
+    },
+  );
+
+  testWidgets(
+    'Can return default mode color',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: LayoutBuilder(
+            builder: (context, constraints) => Icon(
+              Icons.abc,
+              color: ColorGate.byMode(
+                  context: context, lightModeColor: Colors.blue),
+            ),
+          ),
+          theme: ThemeData(
+            brightness: Brightness.light,
+          ),
+        ),
+      );
+
+      expect(
+          (tester.firstWidget(find.byType(Icon)) as Icon).color, Colors.blue);
+    },
+  );
 }
